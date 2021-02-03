afe7e133b5ee551cd49eb7bebf3508826324ed92
lokesh <lokeshmali1997@gmail.com>
2021-02-03 19:42:24 +0530

    commit

---
+++ app/src/main/java/com/example/cutoff/LoginFragment.java
@@ -0,0 +1,66 @@
+package com.example.cutoff;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class LoginFragment extends Fragment {
+
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        Button loginbutton = v.findViewById(R.id.login_button);
+        TextView registation = v.findViewById(R.id.registration);
+
+        registation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                 fragmentTransaction.replace(R.id.maincontainer, new RegistrationFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.maincontainer, new IntroducationFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+
+
+
+        return v;
+    }
+
+}
