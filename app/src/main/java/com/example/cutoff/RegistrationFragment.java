3f157ed1c6814f4d3ed7f2f27d5e3142d23e38f9
shubh <mailshubham8995@gmail.com>
2021-02-05 02:59:16 +0530

    Added library and internet permission

--- app/src/main/java/com/example/cutoff/RegistrationFragment.java
+++ app/src/main/java/com/example/cutoff/RegistrationFragment.java
@@ -9,32 +9,29 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class RegistrationFragment extends Fragment {
-
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_registration, container, false);
-
+        EditText username = v.findViewById(R.id.name);
+        EditText useremail = v.findViewById(R.id.email);
+        EditText userpass = v.findViewById(R.id.password);
+        EditText usernum  = v.findViewById(R.id.mobnumber);
+        Button  register   = v.findViewById(R.id.register_button);
         TextView backlogin = v.findViewById(R.id.textviewLogin);
 
-        backlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                FragmentManager fr = getFragmentManager();
-                FragmentTransaction ft = fr.beginTransaction();
-                ft.replace(R.id.maincontainer,new LoginFragment());
-                ft.commit();
-
-            }
+        backlogin.setOnClickListener(view -> {
+            FragmentManager fr = getFragmentManager();
+            FragmentTransaction ft = fr.beginTransaction();
+            ft.replace(R.id.maincontainer,new LoginFragment());
+            ft.commit();
         });
 
         return v;
