77e368ac4f2a140a2e5594ec3930534160bacb21
Shohrat Ali <shohratali344@gmail.com>
2021-02-12 15:30:20 +0530

    commit ali

--- app/src/main/java/com/example/cutoff/retrofitutil/ApiClient.java
+++ app/src/main/java/com/example/cutoff/retrofitutil/ApiClient.java
@@ -5,11 +5,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    private static final String BASE_URL = " http://localhost/CutOff_API/v1/registerUser/";
+    private static final String BASE_URL = " http://192.168.29.118/CutOff_API/v1/Register/";
     private static Retrofit retrofit = null;
 
 
-    public Retrofit getApiClient() {
+    public static Retrofit getApiClient() {
         if(retrofit == null){
             retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
         }
