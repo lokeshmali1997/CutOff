949c48ca64838104f4a643bf19e90004d518211b
Shohrat Ali <shohratali344@gmail.com>
2021-02-16 14:35:18 +0530

    Login with validation done

--- app/src/main/java/com/example/cutoff/retrofitutil/ApiLoginClient.java
+++ app/src/main/java/com/example/cutoff/retrofitutil/ApiLoginClient.java
@@ -3,13 +3,13 @@ package com.example.cutoff.retrofitutil;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ApiClient {
+public class ApiLoginClient {
 
-    private static final String BASE_URL = " http://192.168.29.118/CutOff_API/v1/Register/";
+    private static final String BASE_URL = " http://192.168.29.118/CutOff_API/v1/Login/";
     private static Retrofit retrofit = null;
 
 
-    public static Retrofit getApiClient() {
+    public static Retrofit getApiLoginClient() {
         if(retrofit == null){
             retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
         }
