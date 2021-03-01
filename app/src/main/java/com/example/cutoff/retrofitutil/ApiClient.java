0c106dbc974a94225f34bf45c0721acf71226209
lokesh <lokeshmali1997@gmail.com>
2021-03-01 11:44:31 +0530

    add course item form

--- app/src/main/java/com/example/cutoff/retrofitutil/ApiClient.java
+++ app/src/main/java/com/example/cutoff/retrofitutil/ApiClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    private static final String BASE_URL = " http://192.168.29.118/CutOff_API/v1/Register/";
+    private static final String BASE_URL = " http://192.168.1.25/CutOff_API/v1/Register/";
     private static Retrofit retrofit = null;
 
 
